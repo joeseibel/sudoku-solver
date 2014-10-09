7efabcfb2208e49387dfc6f1b51fce46db61cbfe
joeseibel <joeseibel@gmail.com>
2014-10-08 22:44:04 -0400

    Implemented Nice Loops Rule 3 for Alternating Inference Chains.

--- SudokuSolver_Java7/src/sudokusolver/solutions/AlternatingInferenceChains.java
+++ SudokuSolver_Java7/src/sudokusolver/solutions/AlternatingInferenceChains.java
@@ -38,6 +38,13 @@ public class AlternatingInferenceChains {
 				}
 			}
 		}
+		if (alternatingInferenceChainsNiceLoopsRule2(puzzle, graph) || alternatingInferenceChainsNiceLoopsRule3(puzzle, graph)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean alternatingInferenceChainsNiceLoopsRule2(Puzzle puzzle, Pseudograph<PossibleNumberInCell, SudokuEdge> graph) {
 		for (PossibleNumberInCell vertex : graph.vertexSet()) {
 			ArrayList<SudokuEdge> strongLinks = new ArrayList<SudokuEdge>();
 			for (SudokuEdge edge : graph.edgesOf(vertex)) {
@@ -48,12 +55,32 @@ public class AlternatingInferenceChains {
 			for (int i = 0; i < strongLinks.size() - 1; i++) {
 				PossibleNumberInCell nextVertex = Common.getOtherVertex(graph, strongLinks.get(i), vertex);
 				for (int j = i + 1; j < strongLinks.size(); j++) {
-					final ArrayDeque<PossibleNumberInCell> cycle = new ArrayDeque<PossibleNumberInCell>();
+					ArrayDeque<PossibleNumberInCell> cycle = new ArrayDeque<PossibleNumberInCell>();
 					cycle.push(vertex);
 					cycle.push(nextVertex);
-					if (findAlternatingLinkCycle(graph, strongLinks.get(j), cycle, nextVertex, false)) {
+					if (findAlternatingLinkCycle(graph, strongLinks.get(j), true, cycle, nextVertex, false)) {
 						assert cycle.size() % 2 == 1;
 						puzzle.setValueAndUpdatePossibleValues(vertex.getCell(), vertex.getPossibleNumber());
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	private static boolean alternatingInferenceChainsNiceLoopsRule3(Puzzle puzzle, Pseudograph<PossibleNumberInCell, SudokuEdge> graph) {
+		for (PossibleNumberInCell vertex : graph.vertexSet()) {
+			SudokuEdge[] edges = graph.edgesOf(vertex).toArray(new SudokuEdge[0]);
+			for (int i = 0; i < edges.length - 1; i++) {
+				PossibleNumberInCell nextVertex = Common.getOtherVertex(graph, edges[i], vertex);
+				for (int j = i + 1; j < edges.length; j++) {
+					ArrayDeque<PossibleNumberInCell> cycle = new ArrayDeque<PossibleNumberInCell>();
+					cycle.push(vertex);
+					cycle.push(nextVertex);
+					if (findAlternatingLinkCycle(graph, edges[j], false, cycle, nextVertex, true)) {
+						assert cycle.size() % 2 == 1;
+						puzzle.removePossibleValue(vertex.getCell(), vertex.getPossibleNumber());
 						return true;
 					}
 				}
@@ -90,13 +117,19 @@ public class AlternatingInferenceChains {
 		}
 	}
 	
-	private static boolean findAlternatingLinkCycle(Pseudograph<PossibleNumberInCell, SudokuEdge> graph, SudokuEdge finalEdge, ArrayDeque<PossibleNumberInCell> cycle,
-			PossibleNumberInCell vertex, boolean nextLinkMustBeStrong) {
+	private static boolean findAlternatingLinkCycle(Pseudograph<PossibleNumberInCell, SudokuEdge> graph, SudokuEdge finalEdge, boolean finalLinkMustBeStrong,
+			ArrayDeque<PossibleNumberInCell> cycle, PossibleNumberInCell vertex, boolean nextLinkMustBeStrong) {
 		ArrayList<PossibleNumberInCell> possibleNextVerticies = new ArrayList<PossibleNumberInCell>();
 		for (SudokuEdge nextEdge : graph.edgesOf(vertex)) {
 			if (nextEdge.equals(finalEdge)) {
-				if (nextLinkMustBeStrong && nextEdge.getLinkType().equals(SudokuEdge.LinkType.STRONG_LINK)) {
-					return true;
+				if (finalLinkMustBeStrong) {
+					if (nextLinkMustBeStrong && nextEdge.getLinkType().equals(SudokuEdge.LinkType.STRONG_LINK)) {
+						return true;
+					}
+				} else {
+					if (cycle.size() % 2 == 1) {
+						return true;
+					}
 				}
 			} else {
 				PossibleNumberInCell nextVertex = Common.getOtherVertex(graph, nextEdge, vertex);
@@ -108,7 +141,7 @@ public class AlternatingInferenceChains {
 		}
 		for (PossibleNumberInCell nextVertex : possibleNextVerticies) {
 			cycle.push(nextVertex);
-			if (findAlternatingLinkCycle(graph, finalEdge, cycle, nextVertex, !nextLinkMustBeStrong)) {
+			if (findAlternatingLinkCycle(graph, finalEdge, finalLinkMustBeStrong, cycle, nextVertex, !nextLinkMustBeStrong)) {
 				return true;
 			}
 			cycle.pop();
