8f5d710df0d3f209d3dece36b004c550be91547a
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2019-03-22 23:23:46 -0400

    Simplified HiddenSingles.

--- SudokuSolver_Kotlin/src/main/sudokusolver/kotlin/logic/HiddenSingles.kt
+++ SudokuSolver_Kotlin/src/main/sudokusolver/kotlin/logic/HiddenSingles.kt
@@ -6,17 +6,14 @@ import sudokusolver.kotlin.SetValue
 import sudokusolver.kotlin.SudokuNumber
 import sudokusolver.kotlin.UnsolvedCell
 
-fun hiddenSingles(board: Board<Cell>): List<SetValue> {
-    val rowModifications = hiddenSingles(board.rows)
-    val columnModifications = hiddenSingles(board.columns)
-    val blockModifications = hiddenSingles(board.blocks)
-    return (rowModifications + columnModifications + blockModifications).distinct()
-}
-
-private fun hiddenSingles(units: List<List<Cell>>): List<SetValue> =
-    units.flatMap { unit ->
-        val unsolved = unit.filterIsInstance<UnsolvedCell>()
-        SudokuNumber.values().mapNotNull { number ->
-            unsolved.filter { number in it.candidates }.takeIf { it.size == 1 }?.let { SetValue(it.first(), number) }
+fun hiddenSingles(board: Board<Cell>): List<SetValue> =
+    (board.rows + board.columns + board.blocks)
+        .flatMap { unit ->
+            val unsolved = unit.filterIsInstance<UnsolvedCell>()
+            SudokuNumber.values().mapNotNull { number ->
+                unsolved.filter { number in it.candidates }
+                    .takeIf { it.size == 1 }
+                    ?.let { SetValue(it.first(), number) }
+            }
         }
-    }
+        .distinct()
