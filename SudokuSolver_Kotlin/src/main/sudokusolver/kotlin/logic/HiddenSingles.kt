ed275c08058145f53eb32f77998e0250444395fa
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2019-03-30 11:44:20 -0400

    Formatting.

--- SudokuSolver_Kotlin/src/main/sudokusolver/kotlin/logic/HiddenSingles.kt
+++ SudokuSolver_Kotlin/src/main/sudokusolver/kotlin/logic/HiddenSingles.kt
@@ -7,13 +7,11 @@ import sudokusolver.kotlin.SudokuNumber
 import sudokusolver.kotlin.UnsolvedCell
 
 fun hiddenSingles(board: Board<Cell>): List<SetValue> =
-    board.units
-        .flatMap { unit ->
-            val unsolved = unit.filterIsInstance<UnsolvedCell>()
-            SudokuNumber.values().mapNotNull { number ->
-                unsolved.filter { number in it.candidates }
-                    .takeIf { it.size == 1 }
-                    ?.let { SetValue(it.first(), number) }
-            }
+    board.units.flatMap { unit ->
+        val unsolved = unit.filterIsInstance<UnsolvedCell>()
+        SudokuNumber.values().mapNotNull { number ->
+            unsolved.filter { number in it.candidates }
+                .takeIf { it.size == 1 }
+                ?.let { SetValue(it.first(), number) }
         }
-        .distinct()
+    }.distinct()
