c12dd3985efef43b895866bd0b6d1b8d6d9399e8
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2021-05-31 21:56:59 -0400

    Added more comments to Empty Rectangles.

--- SudokuSolver_Kotlin/src/main/sudokusolver/kotlin/logic/extreme/EmptyRectangles.kt
+++ SudokuSolver_Kotlin/src/main/sudokusolver/kotlin/logic/extreme/EmptyRectangles.kt
@@ -73,20 +73,47 @@ private fun getIntersections(board: Board<Cell>, candidate: SudokuNumber): List<
             val columnInBlock = column % UNIT_SIZE_SQUARE_ROOT
             val rectangleColumn1 = if (columnInBlock == 0) column + 1 else column - columnInBlock
             val rectangleColumn2 = if (columnInBlock == 2) column - 1 else column - columnInBlock + 2
+            //Check that the rectangle is empty.
             if (candidate !in board[rectangleRow1, rectangleColumn1] &&
                 candidate !in board[rectangleRow1, rectangleColumn2] &&
                 candidate !in board[rectangleRow2, rectangleColumn1] &&
                 candidate !in board[rectangleRow2, rectangleColumn2] &&
+                //Check that at least one cell in the same block and row as the intersection has the candidate.
                 (candidate in board[row, rectangleColumn1] || candidate in board[row, rectangleColumn2]) &&
+                //Check that at least one cell in the same block and column as the intersection has the candidate.
                 (candidate in board[rectangleRow1, column] || candidate in board[rectangleRow2, column])
             ) {
                 row to column
             } else {
+                SolvedCell(0, 0, value = SudokuNumber.ONE)
                 null
             }
         }
     }
 
+/*
+ * Should this be defined here as a private function or should it be a method of Cell and available to all solutions?
+ * The vast majority of solutions first filter by the type UnsolvedCell, and then work with the candidates field of
+ * UnsolvedCell. Empty Rectangles is different because it needs to check that certain cells have a candidate and
+ * certain others do not have a candidate. For cells that don't have a candidate, Empty Rectangles doesn't care if that
+ * is because the cell is solved or if it is an unsolved cell without the candidate. Since this is only really needed by
+ * Empty Rectangles, I decided to have this as a private function here instead of adding it to Cell.
+ *
+ * Is it potentially confusing to override the in operator instead of giving this a name such as hasCandidate? What is
+ * the natural and expected behavior when the cell is a SolvedCell and the candidate is the solution to that cell? Would
+ * someone reasonably expect the expression "SudokuNumber.ONE in SolvedCell(value = SudokuNumber.ONE, ...)" to return
+ * true and be surprised when it actually returns false? It is true that a name like hasCandidate would be more clear,
+ * but since this is a private function and it is easy to see where it is used, I decided to choose brevity over
+ * clarity.
+ *
+ * These questions are not all that interesting here in Kotlin, but will prove to be more interesting in other
+ * languages. Kotlin has the advantage that SolvedCell and UnsolvedCell are their own types, are distinct from each
+ * other, and are distinct from Cell. For languages like Swift and Rust, the Cell type hierarchy will become an enum
+ * with associated values and SolvedCell and UnsolvedCell will become variants of that enum. This has the disadvantage
+ * that our three distinct types in Kotlin will become one type in Swift and Rust. In that case, a method called
+ * hasCandidate will probably be added to the enum. A similar approach might be applied for the Java implementation just
+ * because filtering a Java stream by type is ugly and error-prone.
+ */
 private operator fun Cell.contains(candidate: SudokuNumber): Boolean =
     when (this) {
         is SolvedCell -> false
