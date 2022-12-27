2f3048af117f77b8af13839696c4670bb2b54a72
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2022-12-26 22:56:47 -0800

    Removed unused method.

--- SudokuSolver_Java_No_Streams/src/main/sudokusolver/javanostreams/Triple.java
+++ SudokuSolver_Java_No_Streams/src/main/sudokusolver/javanostreams/Triple.java
@@ -2,29 +2,8 @@ package sudokusolver.javanostreams;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public record Triple<A, B, C>(A first, B second, C third) {
-    public static <T> Collector<T, ?, Stream<Triple<T, T, T>>> zipEveryTriple() {
-        return Collectors.collectingAndThen(
-                Collectors.toList(),
-                list -> IntStream.range(0, list.size() - 2)
-                        .mapToObj(first -> IntStream.range(first + 1, list.size() - 1)
-                                .mapToObj(second -> IntStream.range(second + 1, list.size())
-                                        .mapToObj(third -> new Triple<>(
-                                                list.get(first),
-                                                list.get(second),
-                                                list.get(third)
-                                        )))
-                                .flatMap(Function.identity()))
-                        .flatMap(Function.identity())
-        );
-    }
-
     /*
      * Similar to Pair.zipEveryPair, this method exists to deal with warnings about methods being too complex to analyze
      * by data flow algorithm.
