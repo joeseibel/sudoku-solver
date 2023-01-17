6271e75225862cafe73977cb79280415e02526ba
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2023-01-16 17:54:40 -0800

    Added argument validation for Cell.

--- sudokusolver_scala/src/main/scala/sudokusolver/scala/Cell.scala
+++ sudokusolver_scala/src/main/scala/sudokusolver/scala/Cell.scala
@@ -5,13 +5,17 @@ sealed trait Cell:
   val column: Int
   lazy val block: Int = getBlockIndex(row, column)
 
+  require((0 until UnitSize).contains(row), s"row is $row, must be between 0 and ${UnitSize - 1}.")
+  require((0 until UnitSize).contains(column), s"column is $column, must be between 0 and ${UnitSize - 1}.")
+
 case class SolvedCell(override val row: Int, override val column: Int, value: SudokuNumber) extends Cell
 
 class UnsolvedCell(
                     override val row: Int,
                     override val column: Int,
                     val candidates: Set[SudokuNumber] = SudokuNumber.values.toSet
-                  ) extends Cell
+                  ) extends Cell:
+  require(candidates.nonEmpty, "candidates must not be empty.")
 
 def parseSimpleCells(simpleBoard: String): Board[Cell] =
   require(
--- sudokusolver_scala/src/test/scala/sudokusolver/scala/CellTest.scala
+++ sudokusolver_scala/src/test/scala/sudokusolver/scala/CellTest.scala
@@ -3,6 +3,31 @@ package sudokusolver.scala
 import munit.FunSuite
 
 class CellTest extends FunSuite:
+  test("Cell row too low") {
+    val message = "requirement failed: row is -1, must be between 0 and 8."
+    interceptMessage[IllegalArgumentException](message)(SolvedCell(-1, 0, SudokuNumber.ONE))
+  }
+
+  test("Cell row too high") {
+    val message = "requirement failed: row is 9, must be between 0 and 8."
+    interceptMessage[IllegalArgumentException](message)(SolvedCell(9, 0, SudokuNumber.ONE))
+  }
+
+  test("Cell column too low") {
+    val message = "requirement failed: column is -1, must be between 0 and 8."
+    interceptMessage[IllegalArgumentException](message)(UnsolvedCell(0, -1))
+  }
+
+  test("Cell column too high") {
+    val message = "requirement failed: column is 9, must be between 0 and 8."
+    interceptMessage[IllegalArgumentException](message)(UnsolvedCell(0, 9))
+  }
+
+  test("UnsolvedCell candidates are empty") {
+    val message = "requirement failed: candidates must not be empty."
+    interceptMessage[IllegalArgumentException](message)(UnsolvedCell(0, 0, Set.empty))
+  }
+
   test("parseSimpleCells wrong length") {
     val message = "requirement failed: simpleBoard.length is 0, must be 81."
     interceptMessage[IllegalArgumentException](message)(parseSimpleCells(""))
