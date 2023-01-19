1977f38edc5530a4846da57d1b4fb73375befbdc
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2023-01-18 17:29:10 -0800

    Implemented parseCellsWithCandidates.

--- sudokusolver_scala/src/main/scala/sudokusolver/scala/Cell.scala
+++ sudokusolver_scala/src/main/scala/sudokusolver/scala/Cell.scala
@@ -26,3 +26,32 @@ def parseSimpleCells(simpleBoard: String): Board[Cell] =
     for (cell, columnIndex) <- row.zipWithIndex yield
       if cell == '0' then UnsolvedCell(rowIndex, columnIndex) else SolvedCell(rowIndex, columnIndex, sudokuNumber(cell))
   Board(cells.to(Iterable))
+
+def parseCellsWithCandidates(withCandidates: String): Board[Cell] =
+  var cellBuilders: List[(Int, Int) => Cell] = Nil
+  var index = 0
+  while index < withCandidates.length do
+    val ch = withCandidates(index)
+    if '1' to '9' contains ch then
+      cellBuilders = cellBuilders :+ ((row, column) => SolvedCell(row, column, sudokuNumber(ch)))
+      index = index + 1
+    else if ch == '{' then
+      index = index + 1
+      val closingBrace = withCandidates.indexOf('}', index)
+      require(closingBrace != -1, "Unmatched '{'.")
+      require(closingBrace != index, "Empty \"{}\".")
+      val charsInBrace = (index until closingBrace).map(withCandidates(_))
+      require(!charsInBrace.contains('{'), "Nested '{'.")
+      for charInBrace <- charsInBrace do require('1' to '9' contains charInBrace, s"Invalid character: '$charInBrace'.")
+      val candidates = charsInBrace.map(sudokuNumber).toSet
+      cellBuilders = cellBuilders :+ ((row, column) => UnsolvedCell(row, column, candidates))
+      index = closingBrace + 1
+    else if ch == '}' then
+      throw IllegalArgumentException("Unmatched '}'.")
+    else
+      throw IllegalArgumentException(s"Invalid character: '$ch'.")
+  require(cellBuilders.size == UnitSizeSquared, s"Found ${cellBuilders.size} cells, required $UnitSizeSquared.")
+  val cells = cellBuilders.grouped(UnitSize).zipWithIndex.map { (row, rowIndex) =>
+    row.zipWithIndex.map { (cell, columnIndex) => cell(rowIndex, columnIndex) }
+  }
+  Board(cells.to(Iterable))
--- sudokusolver_scala/src/test/scala/sudokusolver/scala/CellTest.scala
+++ sudokusolver_scala/src/test/scala/sudokusolver/scala/CellTest.scala
@@ -32,3 +32,38 @@ class CellTest extends FunSuite:
     val message = "requirement failed: simpleBoard.length is 0, must be 81."
     interceptMessage[IllegalArgumentException](message)(parseSimpleCells(""))
   }
+
+  test("parseCellsWithCandidates unmatched opening brace") {
+    val message = "requirement failed: Unmatched '{'."
+    interceptMessage[IllegalArgumentException](message)(parseCellsWithCandidates("{"))
+  }
+
+  test("parseCellsWithCandidates empty braces") {
+    val message = "requirement failed: Empty \"{}\"."
+    interceptMessage[IllegalArgumentException](message)(parseCellsWithCandidates("{}"))
+  }
+
+  test("parseCellsWithCandidates nested brace") {
+    val message = "requirement failed: Nested '{'."
+    interceptMessage[IllegalArgumentException](message)(parseCellsWithCandidates("{{}"))
+  }
+
+  test("parseCellsWithCandidates invalid character in braces") {
+    val message = "requirement failed: Invalid character: 'a'."
+    interceptMessage[IllegalArgumentException](message)(parseCellsWithCandidates("{a}"))
+  }
+
+  test("parseCellsWithCandidates unmatched closing brace") {
+    val message = "Unmatched '}'."
+    interceptMessage[IllegalArgumentException](message)(parseCellsWithCandidates("}"))
+  }
+
+  test("parseCellsWithCandidates invalid character") {
+    val message = "Invalid character: 'a'."
+    interceptMessage[IllegalArgumentException](message)(parseCellsWithCandidates("a"))
+  }
+
+  test("parseCellsWithCandidates wrong length") {
+    val message = "requirement failed: Found 0 cells, required 81."
+    interceptMessage[IllegalArgumentException](message)(parseCellsWithCandidates(""))
+  }
