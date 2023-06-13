12d780386651184b26c71645028aa781b791d55a
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2023-06-12 23:32:09 -0700

    Implemented YWing.

--- sudokusolver_scala/src/main/scala/sudokusolver/scala/SudokuSolver.scala
+++ sudokusolver_scala/src/main/scala/sudokusolver/scala/SudokuSolver.scala
@@ -3,7 +3,7 @@ package sudokusolver.scala
 import sudokusolver.scala.logic.BruteForceSolution.{MultipleSolutions, NoSolutions, SingleSolution}
 import sudokusolver.scala.logic.bruteForce
 import sudokusolver.scala.logic.simple.*
-import sudokusolver.scala.logic.tough.{simpleColoringRule2, simpleColoringRule4, xWing}
+import sudokusolver.scala.logic.tough.{simpleColoringRule2, simpleColoringRule4, xWing, yWing}
 
 import scala.annotation.tailrec
 
@@ -97,6 +97,7 @@ private def performNextSolution(board: Board[Cell]): Seq[BoardModification] =
     //Start of tough solutions.
     xWing,
     simpleColoringRule2,
-    simpleColoringRule4
+    simpleColoringRule4,
+    yWing
   )
   solutions.map(_(board)).find(_.nonEmpty).getOrElse(LazyList.empty)
---
+++ sudokusolver_scala/src/main/scala/sudokusolver/scala/logic/tough/YWing.scala
@@ -0,0 +1,36 @@
+package sudokusolver.scala.logic.tough
+
+import sudokusolver.scala.{Board, Cell, RemoveCandidates, UnsolvedCell, mergeToRemoveCandidates, zipEveryTriple}
+
+/*
+ * http://www.sudokuwiki.org/Y_Wing_Strategy
+ *
+ * Given a hinge cell and two wing cells such that the hinge can see both wings, all three cells each have two
+ * candidates, there are three total candidates across the three cells, the hinge shares one candidate with one wing and
+ * one candidate with the other wing, and the wing cells share a candidate among each other, then this third candidate
+ * must be the solution to one of the wings. The third candidate can be removed from any cell which can see both wings.
+ */
+def yWing(board: Board[Cell]): Seq[RemoveCandidates] =
+
+  def tryHinge(hinge: UnsolvedCell, wingA: UnsolvedCell, wingB: UnsolvedCell) =
+    val wingCandidates = wingA.candidates & wingB.candidates
+    if hinge.isInSameUnit(wingA) && hinge.isInSameUnit(wingB) &&
+      (hinge.candidates & wingA.candidates).size == 1 &&
+      (hinge.candidates & wingB.candidates).size == 1 &&
+      wingCandidates.size == 1 then
+      val candidate = wingCandidates.head
+      for
+        cell <- board.cells.collect { case cell: UnsolvedCell => cell }
+        if cell != wingA && cell != wingB &&
+          cell.candidates.contains(candidate) &&
+          cell.isInSameUnit(wingA) && cell.isInSameUnit(wingB)
+      yield (cell, candidate)
+    else
+      Seq.empty
+
+  board.cells
+    .collect { case cell: UnsolvedCell if cell.candidates.size == 2 => cell }
+    .zipEveryTriple
+    .filter((a, b, c) => (a.candidates | b.candidates | c.candidates).size == 3)
+    .flatMap((a, b, c) => tryHinge(a, b, c) ++ tryHinge(b, a, c) ++ tryHinge(c, a, b))
+    .mergeToRemoveCandidates
