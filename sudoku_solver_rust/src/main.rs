83d353a4e106e6d765176cb4fa13190c5a1c6293
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2024-12-13 22:32:11 -0800

    Created SudokuNumber enum.

--- sudoku_solver_rust/src/main.rs
+++ sudoku_solver_rust/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, Sudoku Solver!");
+    println!("{:?}", SudokuNumber::One);
+    println!("{:?}", SudokuNumber::Two);
+    println!("{:?}", SudokuNumber::Three);
+    println!("{:?}", SudokuNumber::Four);
+    println!("{:?}", SudokuNumber::Five);
+    println!("{:?}", SudokuNumber::Six);
+    println!("{:?}", SudokuNumber::Seven);
+    println!("{:?}", SudokuNumber::Eight);
+    println!("{:?}", SudokuNumber::Nine);
+}
+
+// TODO: Remove Debug trait after removing println! statements from main.
+#[derive(Debug)]
+enum SudokuNumber {
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
 }
