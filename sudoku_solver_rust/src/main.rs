6eaca4505b862698f67e2ac46dd35159b8ea3d91
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2025-03-26 17:25:02 -0700

    Use an array instead of a Vec.

--- sudoku_solver_rust/src/main.rs
+++ sudoku_solver_rust/src/main.rs
@@ -115,7 +115,7 @@ fn solve(input: Board<Option<SudokuNumber>>) -> Result<Board<SudokuNumber>, Solv
 }
 
 fn perform_next_solution(board: &Board<Cell>) -> Vec<BoardModification> {
-    let solutions = vec![
+    let solutions = [
         prune_candidates::prune_candidates,
         naked_singles::naked_singles,
         hidden_singles::hidden_singles,
