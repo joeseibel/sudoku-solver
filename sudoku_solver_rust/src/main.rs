fde2f548d686f0f91ebc47e5aa6d5f998d77033e
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2025-12-23 18:09:51 -0800

    Wrote better comment about closure lifetime issue.

--- sudoku_solver_rust/src/main.rs
+++ sudoku_solver_rust/src/main.rs
@@ -148,39 +148,28 @@ fn perform_next_solution(board: &Board<Cell>) -> Vec<BoardModification> {
         x_cycles::x_cycles_rule_1,
         x_cycles::x_cycles_rule_2,
         x_cycles::x_cycles_rule_3,
-        // Something strange is going on with the type of board in the following closure and I don't really understand
-        // it. If I don't explicity specify its type, the compiler correctly infers it to be a &Board<Cell>, but places
-        // a mismatched types error on the entire closure with the confusing error message, "one type is more general
-        // than the other." The error also provides this note on the expected and found types:
+        // The following closure runs into an interesting lifetime issue. Since the closure takes a reference as its
+        // parameter, the compiler must infer what the lifetime of the reference should be. The compiler could choose a
+        // higher-ranked lifetime, meaning that the closure could accept a &Board with any lifetime. The compiler could
+        // also choose a local lifetime. The lifetime must also be consistent with the lifetimes for all of the other
+        // function pointers that are stored in the variable solutions.
+        //
+        // If I don't specify the type of board, but allow it to be inferred to be a &Board<Cell>, then the compiler
+        // will infer a local lifetime. This causes an error because the solutions array expects function pointers with
+        // a higher-ranked lifetime. The error has the message, "one type is more general than the other." It also
+        // provides this note on the expected and found types:
         //
         // = note: expected fn pointer `for<'a> fn(&'a board::Board<_>) -> std::vec::Vec<_>`
         //            found fn pointer `fn(&board::Board<_>) -> std::vec::Vec<_>`
         //
-        // I have no clue what this is complaining about. It does seem to be related to lifetimes and a quick Google
-        // search shows that I'm not the only one who is confused. The most helpful explaination I found was in the
-        // following post:
-        // https://users.rust-lang.org/t/why-does-one-type-is-more-general-than-other-occur-with-the-same-type/69150/4
+        // If I do specify the type of board, then the compiler correctly inferrs the lifetime to be higher-ranked and
+        // the closure sucessfully compiles.
         //
-        // In that post, the user quinedot said, "Errors like this can happen when you have a closure that is inferred
-        // to be tied to a specific lifetime instead of higher-ranked over a generic lifetime. It's a known short-coming
-        // of the current implementation, and yes, the error is confusing. The 'more general than the other' is a hint
-        // that higher-ranked types are involved."
+        // There is experimental syntax for explicitly specifying higher-ranked lifetimes in closures. See the RFC for
+        // this feature: https://rust-lang.github.io/rfcs/3216-closure-lifetime-binder.html
         //
-        // I also discovered a number of GitHub issues related to higher-ranked lifetime errors as well as the "one type
-        // is more general than the other" error message: https://github.com/rust-lang/rust/issues/110338
-        //
-        // Adding an explicit type for board solves the error, but I really don't understand why, especially since I
-        // didn't have to specify a general lifetime. I would like to dig into this further to have a better
-        // understanding of the lifetime issues here, especially if the compiler is updated to provide a more meaningful
-        // error message.
-        //
-        // Additional note: Rust has experimental syntax for specifying a lifetime for a closure. See the following
-        // links:
-        // https://github.com/rust-lang/rust/issues/97362
-        // https://github.com/rust-lang/rfcs/pull/3216
-        // https://internals.rust-lang.org/t/pre-rfc-allow-for-a-syntax-with-closures-for-explicit-higher-ranked-lifetimes/15888
-        //
-        // TODO: Read through the above links to have a better understanding of the issue and then rewrite this comment.
+        // Even though I don't use the lifetime binder, I found the RFC to be incredibly helpful for understanding this
+        // lifetime issue.
         |board: &Board<_>| bug::bug(board).into_iter().collect(),
     ];
     solutions
