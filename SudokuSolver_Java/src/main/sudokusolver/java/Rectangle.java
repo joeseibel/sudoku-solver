54d96459a3dcdb4b0960bc3d4c677108ec7a86f7
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2026-03-14 18:36:15 -0700

    The type inference problem in Rectangle has been resolved.

--- SudokuSolver_Java/src/main/sudokusolver/java/Rectangle.java
+++ SudokuSolver_Java/src/main/sudokusolver/java/Rectangle.java
@@ -51,33 +51,9 @@ public class Rectangle {
                                 var cellD = board.get(rowB, columnB);
                                 return Stream.of(cellA, cellB, cellC, cellD)
                                         .filter(UnsolvedCell.class::isInstance)
-                                        .map(UnsolvedCell.class::cast);
+                                        .map(UnsolvedCell.class::cast)
+                                        .toList();
                             })
-                            /*
-                             * Something weird is happening here with the Java compiler keeping track of the types
-                             * through this chain. I initially wanted to put the following toList conversion within and
-                             * at the end of the previous map. However, even though the IDE is happy with that, the Java
-                             * compiler fails at the end of this chain when constructing a new Rectangle.
-                             *
-                             * The compiler error that shows up at the end of this chain is the following
-                             *     java: incompatible types: invalid constructor reference
-                             *         incompatible types: java.lang.Object cannot be converted to java.util.List<sudokusolver.java.UnsolvedCell>
-                             *
-                             * Obviously, the compiler has somehow lost the type of the stream at
-                             * ".map(Rectangle::new)". However, if I don't use a method reference, but instead have a
-                             * normal constructor call in a normal lambda, then the compiler is happy. But in that case,
-                             * the IDE then gives a warning stating that the lambda can be converted to a method
-                             * reference.
-                             *
-                             * Also, I can explicitly add type information to the above map so that it looks like this:
-                             * ".<List<UnsolvedCell>>map(columnIndices -> ...". In that case, the compiler is happy, but
-                             * the IDE gives a warning stating that the types can be inferred.
-                             *
-                             * It would be worthwhile to do two things about this. First, I should switch from using the
-                             * Azul Zulu compiler and see if this happens with OpenJDK. Secondly, if the problem
-                             * persists with OpenJDK, then I should create a minimal example and file a bug.
-                             */
-                            .map(Stream::toList)
                             .filter(cells -> cells.size() == 4)
                             .map(Rectangle::new);
                 })
